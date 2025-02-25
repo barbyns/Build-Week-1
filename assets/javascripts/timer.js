4bd9d0359789017b1518d53c259cb1c717bbb5ce
Silvia <silvia8gasparini@gmail.com>
2025-02-25 15:54:22 +0100

    Add timer js

---
+++ assets/javascripts/timer.js
@@ -0,0 +1,47 @@
+let currentQuest = 0; // Indice della domanda corrente
+  let score = 0; // Punteggio totale dell'utente
+  let timer; // Variabile per il timer
+  let timeLeft = 60; // Tempo rimanente per ogni domanda
+
+  // Funzione per avviare il timer. Va creato un <div id="timer">60</div> nella pagina HTML
+  const goTimer = function() {
+    clearInterval(timer); // Ferma il timer precedente
+      timeLeft = 60; // Reimposta il timer a 60 sec 
+      document.getElementById("timer").textContent = `Tempo rimasto: ${timeLeft}s`;
+      timer = setInterval(() => { // setInterval viene usato per far funzionare il timer che conta i secondi rimanenti per rispondere a ogni domanda 
+          timeLeft = timeLeft - 1;
+          document.getElementById("timer").textContent = `Tempo rimasto: ${timeLeft}s`;
+          if (timeLeft === 0) {
+              nextQuest(); // Passa automaticamente alla domanda successiva se il tempo scade
+          }
+      }, 1000);
+  }
+    
+// Funzione per caricare una nuova domanda. DA COMPLETARE perché bisogna prendere la domanda dall'array.
+const loadQuestion = function() {
+    if (currentQuest >= questions.length) {
+        endQuiz(); // Se non ci sono più domande, termina il quiz
+        return;
+    }
+
+    goTimer(); // Avvia il timer per la nuova domanda
+};
+
+
+// Funzione per passare alla domanda successiva
+const nextQuest = (points = 0) => {
+    score = score + points; // Se il tempo è scaduto, aggiunge 0 punti
+    currentQuest = currentQuest + 1 // Incrementa indice della domanda
+    if (currentQuest >= questions.length) {
+        endQuiz(); // Se finite le domande, chiude il quiz
+    } else {
+         loadQuestion(); // Altrimenti carica la prossima domanda
+    }
+};
+
+  // Funzione che termina il quiz e manda alla pagina dei risultati
+ const endQuiz = function () {
+      clearInterval(timer); // Ferma il timer
+      window.location.href = "results.html" + score; // Reindirizza alla pagina dei risultati con il punteggio
+  }
+
