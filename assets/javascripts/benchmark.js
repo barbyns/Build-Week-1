21731c9472ff9d0b98087a1158a895b1ffbe3cb7
sanandreas11 <andreaandonis@outlook.com>
2025-02-25 09:54:14 +0100

    updates js

--- assets/javascripts/benchmark.js
+++ assets/javascripts/benchmark.js
@@ -1,14 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const options = document.querySelectorAll(".option");
-    const correctAnswer = "<input type='checkbox'>";
+  const options = document.querySelectorAll(".option")
+  const correctAnswer = "<input type='checkbox'>"
 
-    options.forEach(option => {
-        option.addEventListener("click", function () {
-            if (option.textContent.trim() === correctAnswer) {
-                alert("Correct answer!");
-            } else {
-                alert("Wrong answer! Try again.");
-            }
-        });
-    });
-});
+  options.forEach((option) => {
+    option.addEventListener("click", function () {
+      if (option.textContent.trim() === correctAnswer) {
+        alert("Correct answer!")
+      } else {
+        alert("Wrong answer! Try again.")
+      }
+    })
+  })
+})
+
+const result = 0 //qui ci dovrà essere una function che prende le risposte dal quiz
